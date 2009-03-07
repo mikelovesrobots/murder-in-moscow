387ade53ad421cba60f0e0bcde55c4bef4e32078
Mike Judge <mike@activerain.com>
2009-03-07 11:59:21 -0800

    Added a messages, map and status window.  Ugly code.

--- init.rb
+++ init.rb
@@ -2,11 +2,6 @@ require 'ncurses'
 require 'lib/mim'
 
 Mim.initialize_ncurses do |window|
-  "Murder in Moscow".each_byte do |ch|
-    window.addch ch
-    window.refresh
-    Ncurses.napms 100 # refreshes every 1/10th of a second
-  end
-  window.getch
+  Mim.initialize_game(window)
 end
 
--- lib/mim.rb
+++ lib/mim.rb
@@ -12,11 +12,49 @@ module Mim
     begin
       window = Ncurses.initscr
       Ncurses.cbreak
-
+      
+      Ncurses.start_color
+      Ncurses.init_pair 1, Ncurses::COLOR_WHITE, Ncurses::COLOR_BLACK
+      Ncurses.init_pair 2, Ncurses::COLOR_YELLOW, Ncurses::COLOR_BLACK
+      Ncurses.init_pair 3, Ncurses::COLOR_YELLOW, Ncurses::COLOR_BLUE
+      Ncurses.init_pair 4, Ncurses::COLOR_YELLOW, Ncurses::COLOR_GREEN
+     
       yield window
     ensure
       Ncurses.endwin
     end
+  end
+
+  def self.initialize_game(window)
+    messages_window = window.subwin(4,max_columns(window),0,0) # h, w, y, x
+    messages_window.bkgd Ncurses.COLOR_PAIR(2)
+    messages_window.addstr "Hello, from the messages window."
+
+    map_window = window.subwin(27,90,4,1)
+    map_window.bkgd Ncurses.COLOR_PAIR(3)
+    map_window.addstr "Hello from the map window"
+
+    status_window = window.subwin(27, (max_columns(window) - 90 - 3), 4, 92)
+    status_window.bkgd Ncurses.COLOR_PAIR(4)
+    status_window.addstr "Hello from the status window"
+    #window.addstr "Hello from the top window"
+    window.refresh
+    window.getch
+  end
+
+  def self.max_lines(window)
+    lines = []
+    columns = []
+    window.getmaxyx(lines, columns)
+
+    lines.first
+  end
+
+  def self.max_columns(window)
+    lines = []
+    columns = []
+    window.getmaxyx(lines, columns)
+    columns.first
   end
 end
 
