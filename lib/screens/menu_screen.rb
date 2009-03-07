656cc5f4081ec48211aec1c4b5445da5004dbc31
Mike Judge <mike@activerain.com>
2009-03-07 14:44:33 -0800

    Removed unnecessary attr_accessor

--- lib/screens/menu_screen.rb
+++ lib/screens/menu_screen.rb
@@ -1,13 +1,11 @@
 class MenuScreen < MimScreen::Base
-  attr_accessor :screen
-
   def after_render_screen
     center(12, "Murder in Moscow")
     center(16, "( Press a key to begin )")
   end
 
   def main_loop(screen)
-    screen.getch # wait for a keystroke
+    # wait for a keystroke
+    screen.getch 
   end
-
 end
